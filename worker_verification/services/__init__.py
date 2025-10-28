20bc499161234cd3affb4eedcd09ae867418aaa4
zXpect <zzzxpect2@gmail.com>
2025-10-28 09:40:09 -0500

    feat(bulk_worker): Add services for bulk data upload

--- worker_verification/services/__init__.py
+++ worker_verification/services/__init__.py
@@ -2,10 +2,12 @@ from .firebase_service import firebase_service
 from .worker_service import worker_service
 from .document_service import document_service
 from .client_service import client_service
+from .bulk_worker_service import bulk_worker_service
 
 __all__ = [
     'firebase_service',
     'worker_service',
     'document_service',
     'client_service',
+    'bulk_worker_service',
 ]
